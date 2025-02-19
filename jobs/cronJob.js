8ca1c6c4ebc84a063d7a4f7b77c59f15b00acc0f
Ravinder <rvnegi786@gmail.com>
2025-02-19 15:46:24 +0530

    added corn job for deletion of expired groups

--- jobs/cronJob.js
+++ jobs/cronJob.js
@@ -1,7 +1,14 @@
 const cron = require("node-cron");
+const { deleteExpiredGroup } = require("../model/group.model");
 
-// Cron job to run every 5 minutes
-cron.schedule("*/5 * * * *", async () => {
-  console.log("Running cron job to clean up trash files...");
-  // return
+// Cron job to run at 00:00 (midnight) every day
+cron.schedule("0 0 * * *", async () => {
+  try {
+    console.log("Cron job scheduled to run every night at 00:00 hrs.");
+    console.log("Running cron job to clean up expired groups at midnight...");
+    await deleteExpiredGroup();
+    console.log("Deleted expired groups");
+  } catch (error) {
+    console.log("Error in deleting expired groups:", error.message);
+  }
 });
